a089424794e96b5baf2d737be3d90b2d000f543d
Ayu <hello@ayuhito.com>
2023-10-04 18:00:27 +0100

    feat: add basic logger

--- core/go.mod
+++ core/go.mod
@@ -1,3 +1,10 @@
 module github.com/medama-io/medama
 
 go 1.21.1
+
+require (
+	github.com/lmittmann/tint v1.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
---
+++ core/util/logger.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/lmittmann/tint"
+	"github.com/mattn/go-colorable"
+)
+
+type Logger struct {
+	*slog.Logger
+}
+
+// NewHandler creates a new slog handler.
+func NewHandler(w io.Writer, isDebug bool) slog.Handler {
+	logLevel := &slog.LevelVar{}
+
+	handlerOpts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	var handler slog.Handler
+	switch getAppEnv() {
+	case "production":
+		handler = slog.NewJSONHandler(w, handlerOpts)
+	case "development":
+		// Use tint handler for pretty readable logs
+		tintOpts := &tint.Options{
+			Level: logLevel,
+		}
+
+		if w == os.Stdout {
+			handler = tint.NewHandler(colorable.NewColorableStdout(), tintOpts)
+		} else {
+			handler = slog.NewTextHandler(w, handlerOpts)
+		}
+	default:
+		handler = slog.NewTextHandler(w, handlerOpts)
+	}
+
+	if isDebug {
+		logLevel.Set(slog.LevelDebug)
+	}
+
+	return handler
+}
+
+// Helper function to get app env from env var before initializing config used in logger.
+func getAppEnv() string {
+	appEnv := os.Getenv("APP_ENV")
+	if appEnv == "" {
+		return "development"
+	}
+
+	return appEnv
+}
