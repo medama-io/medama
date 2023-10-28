39a6b597d124e52fcc7f653f58afd9771c32540b
Ayu <hello@ayuhito.com>
2023-10-28 22:52:19 +0100

    fix(core): update 404 handler to match existing error format

--- core/middlewares/404.go
+++ core/middlewares/404.go
@@ -7,25 +7,34 @@ import (
 	"github.com/go-faster/jx"
 )
 
-func NotFound() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
+const errMessage = "api path not found"
+
+func NotFound() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Header().Set("Content-Type", "application/json")
 
 		e := jx.GetEncoder()
 		e.ObjStart()
 		e.FieldStart("error")
-		e.StrEscape("not found")
+		e.ObjStart()
+		e.FieldStart("code")
+		e.Int(http.StatusNotFound)
+		e.FieldStart("message")
+		e.StrEscape(errMessage)
+		e.ObjEnd()
 		e.ObjEnd()
 
 		_, _ = w.Write(e.Bytes())
 
 		attributes := []slog.Attr{
-			slog.String("path", r.URL.Path),
-			slog.String("method", r.Method),
+			slog.String("path", req.URL.Path),
+			slog.String("method", req.Method),
 			slog.Int("status_code", http.StatusNotFound),
+			slog.String("message", errMessage),
+			slog.String("User-Agent", req.Header.Get("User-Agent")),
 		}
 
-		slog.LogAttrs(r.Context(), slog.LevelInfo, "Not Found", attributes...)
+		slog.LogAttrs(req.Context(), slog.LevelInfo, "Not Found", attributes...)
 	}
 }
