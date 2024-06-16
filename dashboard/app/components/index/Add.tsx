c9240b5b8ae8f43108f559246fc20a2681ec3924
Ayuhito <hello@ayuhito.com>
2024-06-16 16:40:00 +0100

    feat: add form handling for add website

--- dashboard/app/components/index/Add.tsx
+++ dashboard/app/components/index/Add.tsx
@@ -6,20 +6,37 @@ import {
 	TextInput,
 	UnstyledButton,
 } from '@mantine/core';
-import { Form } from '@remix-run/react';
-import { TimezoneSelect } from './TimezoneSelect';
-
+import { useForm } from '@mantine/form';
+import { Form, useSubmit } from '@remix-run/react';
+import { zodResolver } from 'mantine-form-zod-resolver';
 import { useState } from 'react';
 import { useTimezoneSelect, type ITimezone } from 'react-timezone-select';
+import isFQDN from 'validator/lib/isFQDN';
+import { z } from 'zod';
+
+import { IconArrowRight } from '@/components/icons/arrow-right';
+
 import classes from './Add.module.css';
-import { IconArrowRight } from '../icons/arrow-right';
+import { TimezoneSelect } from './TimezoneSelect';
 
 interface AddProps {
 	close: () => void;
 }
 
+const addWebsiteSchema = z.object({
+	hostname: z.string().refine((value) => isFQDN(value), {
+		message: 'Please enter a valid domain name.',
+	}),
+});
+
 export const Add = ({ close }: AddProps) => {
-	const [hostname, setHostname] = useState('');
+	const submit = useSubmit();
+	const form = useForm({
+		mode: 'uncontrolled',
+		initialValues: { hostname: '' },
+		validateInputOnBlur: true,
+		validate: zodResolver(addWebsiteSchema),
+	});
 
 	const { options: tzOptions, parseTimezone } = useTimezoneSelect({
 		labelStyle: 'abbrev',
@@ -30,8 +47,13 @@ export const Add = ({ close }: AddProps) => {
 	);
 
 	const resetAndClose = () => {
-		setHostname('');
+		form.reset();
 		close();
+	};
+
+	const handleSubmit = (values: typeof form.values) => {
+		submit(values, { method: 'POST' });
+		resetAndClose();
 	};
 
 	return (
@@ -47,17 +69,16 @@ export const Add = ({ close }: AddProps) => {
 			<Text size="sm" mt="xs">
 				Tell us more about your website so we can add it to your dashboard.
 			</Text>
-			<Form method="post">
+			<Form onSubmit={form.onSubmit(handleSubmit)}>
 				<TextInput
-					name="hostname"
 					label="Domain Name"
 					placeholder="yourwebsite.com"
 					description="The domain or subdomain name of your website."
+					key={form.key('hostname')}
+					{...form.getInputProps('hostname')}
 					classNames={{ input: classes.input }}
 					required
 					mt="md"
-					onChange={(e) => setHostname(e.currentTarget.value)}
-					value={hostname}
 					autoComplete="off"
 					data-autofocus
 				/>
