e56903e083edbc175e502fa41d9beb2520a29251
Ayuhito <hello@ayuhito.com>
2024-06-23 14:32:09 +0100

    fix: move user agent checks earlier in process

--- core/services/event.go
+++ core/services/event.go
@@ -97,6 +97,41 @@ func (h *Handler) PostEventHit(ctx context.Context, req api.EventHit, params api
 			return ErrNotFound(model.ErrWebsiteNotFound), nil
 		}
 
+		// Get request from context
+		reqBody, ok := ctx.Value(model.RequestKeyBody).(*http.Request)
+		if !ok {
+			log.Error().Msg("hit: failed to get request key from context")
+			return ErrInternalServerError(model.ErrRequestContext), nil
+		}
+
+		// Parse user agent first to catch early if it is a bot.
+		rawUserAgent := reqBody.Header.Get("User-Agent")
+		ua := h.useragent.Parse(rawUserAgent)
+
+		uaBrowser := ua.Browser
+		if uaBrowser == "" {
+			uaBrowser = Unknown
+		}
+
+		uaOS := ua.OS
+		if uaOS == "" {
+			uaOS = Unknown
+		}
+
+		uaDevice := Unknown
+		switch {
+		case ua.Desktop:
+			uaDevice = "Desktop"
+		case ua.Mobile:
+			uaDevice = "Mobile"
+		case ua.Tablet:
+			uaDevice = "Tablet"
+		case ua.TV:
+			uaDevice = "TV"
+		case ua.Bot: // If the user agent is a bot, we want to ignore it.
+			return &api.PostEventHitNoContent{}, nil
+		}
+
 		// Parse referrer URL and remove any query parameters or self-referencing
 		// hostnames.
 		var referrerHost string
@@ -134,13 +169,6 @@ func (h *Handler) PostEventHit(ctx context.Context, req api.EventHit, params api
 			}
 		}
 
-		// Get request from context
-		reqBody, ok := ctx.Value(model.RequestKeyBody).(*http.Request)
-		if !ok {
-			log.Error().Msg("hit: failed to get request key from context")
-			return ErrInternalServerError(model.ErrRequestContext), nil
-		}
-
 		// Get users language from Accept-Language header
 		languages, _, err := language.ParseAcceptLanguage(reqBody.Header.Get("Accept-Language"))
 		if err != nil {
@@ -155,34 +183,6 @@ func (h *Handler) PostEventHit(ctx context.Context, req api.EventHit, params api
 			base, _ := languages[0].Base()
 			languageBase = display.English.Tags().Name(language.Make(base.String()))
 			languageDialect = display.English.Tags().Name(languages[0])
-		}
-
-		// Parse user agent
-		rawUserAgent := reqBody.Header.Get("User-Agent")
-		ua := h.useragent.Parse(rawUserAgent)
-
-		uaBrowser := ua.Browser
-		if uaBrowser == "" {
-			uaBrowser = Unknown
-		}
-
-		uaOS := ua.OS
-		if uaOS == "" {
-			uaOS = Unknown
-		}
-
-		uaDevice := Unknown
-		switch {
-		case ua.Desktop:
-			uaDevice = "Desktop"
-		case ua.Mobile:
-			uaDevice = "Mobile"
-		case ua.Tablet:
-			uaDevice = "Tablet"
-		case ua.TV:
-			uaDevice = "TV"
-		case ua.Bot: // If the user agent is a bot, we want to ignore it.
-			return &api.PostEventHitNoContent{}, nil
 		}
 
 		// Get utm source, medium, and campaigm from URL query parameters.
