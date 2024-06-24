144f868098509084181f7f31ea9ed3d4d6638d12
Ayuhito <hello@ayuhito.com>
2024-06-24 19:42:03 +0100

    feat: add filter select

--- dashboard/app/components/stats/StatsDisplay.tsx
+++ dashboard/app/components/stats/StatsDisplay.tsx
@@ -1,22 +1,60 @@
-import { Group, Tabs, Text, UnstyledButton } from '@mantine/core';
+import { Box, Group, Tabs, Text, UnstyledButton } from '@mantine/core';
 import { Link, useSearchParams } from '@remix-run/react';
 
 import { formatCount, formatDuration } from './formatter';
 import classes from './StatsDisplay.module.css';
 
-interface StatsItemProps {
+interface StatsItem {
 	label: string;
 	count: number | undefined;
 	percentage: number | undefined;
-	isTime?: boolean;
 }
 
-const StatsItem = ({ label, count, percentage, isTime }: StatsItemProps) => {
-	const formattedValue = isTime
-		? formatDuration(count ?? 0)
-		: formatCount(count ?? 0);
+interface StatsItemProps extends StatsItem {
+	tab: string;
+}
+
+export interface StatsTab {
+	label: string;
+	items: StatsItem[];
+}
+
+interface StatsDisplayProps {
+	data: StatsTab[];
+}
+
+const StatsItem = ({ label, count, percentage, tab }: StatsItemProps) => {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const formattedValue =
+		tab === 'Time' ? formatDuration(count ?? 0) : formatCount(count ?? 0);
+
+	const handleFilter = () => {
+		if (tab !== 'Time') {
+			const params = new URLSearchParams(searchParams);
+
+			const filterMap: Record<string, string> = {
+				Referrers: 'referrer',
+				Sources: 'utm_source',
+				Mediums: 'utm_medium',
+				Campaigns: 'utm_campaign',
+				Browsers: 'browser',
+				OS: 'os',
+				Devices: 'device',
+				Countries: 'country',
+				Languages: 'language',
+			};
+			const filter = filterMap[tab] ?? 'path';
+
+			params.append(`${filter}[eq]`, label);
+			setSearchParams(params, {
+				preventScrollReset: true,
+			});
+		}
+	};
+
 	return (
-		<div className={classes['stat-item']}>
+		<UnstyledButton className={classes['stat-item']} onClick={handleFilter}>
 			<Group justify="space-between" pb={6}>
 				<Text fz={14}>{label}</Text>
 				<Text fw={600} fz={14}>
@@ -27,18 +65,9 @@ const StatsItem = ({ label, count, percentage, isTime }: StatsItemProps) => {
 				className={classes.bar}
 				style={{ width: `${(percentage ?? 0) * 100}%` }}
 			/>
-		</div>
+		</UnstyledButton>
 	);
 };
-
-export interface StatsTab {
-	label: string;
-	items: StatsItemProps[];
-}
-
-interface StatsDisplayProps {
-	data: StatsTab[];
-}
 
 export const StatsDisplay = ({ data }: StatsDisplayProps) => {
 	const [searchParams] = useSearchParams();
@@ -64,11 +93,7 @@ export const StatsDisplay = ({ data }: StatsDisplayProps) => {
 				<Tabs.Panel key={tab.label} value={tab.label}>
 					<div style={{ minHeight: 280 }}>
 						{tab.items.map((item) => (
-							<StatsItem
-								key={item.label}
-								isTime={tab.label === 'Time'}
-								{...item}
-							/>
+							<StatsItem key={item.label} tab={tab.label} {...item} />
 						))}
 					</div>
 					<div className={classes.more}>
