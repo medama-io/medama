01c4051d0964f6d947c83490742985da3256edb0
Ayuhito <hello@ayuhito.com>
2024-06-26 21:11:23 +0100

    perf: memoize calculations

--- dashboard/app/components/stats/Chart.tsx
+++ dashboard/app/components/stats/Chart.tsx
@@ -1,7 +1,8 @@
 import { BarChart as MantineBarChart } from '@mantine/charts';
+import { ColorSwatch, Group, Paper, Text } from '@mantine/core';
 import { useSearchParams } from '@remix-run/react';
 import { format, parseISO } from 'date-fns';
-import { ColorSwatch, Group, Paper, Text } from '@mantine/core';
+import React, { useMemo } from 'react';
 
 import { formatCount, formatDuration, formatPercentage } from './formatter';
 
@@ -24,10 +25,21 @@ interface TooltipPayload {
 interface ChartTooltipProps {
 	label: string;
 	date: string;
-	period: string | null;
+	period: Period | null;
 	payload: TooltipPayload[];
 	valueFormatter: (value: number) => string;
 }
+
+type HourPeriod = `${number}h`;
+type DayPeriod = `${number}d`;
+type FixedPeriod = 'today' | 'yesterday' | 'quarter';
+type Period = HourPeriod | DayPeriod | FixedPeriod;
+
+const PERIODS = {
+	TODAY: 'today',
+	YESTERDAY: 'yesterday',
+	QUARTER: 'quarter',
+} as const;
 
 const intlFormatterBasic = new Intl.DateTimeFormat('en', {
 	dateStyle: 'long',
@@ -44,77 +56,75 @@ const intlFormatterAll = new Intl.DateTimeFormat('en', {
 	hour: 'numeric',
 });
 
-const ChartTooltip = ({
-	label,
-	date,
-	period,
-	payload,
-	valueFormatter,
-}: ChartTooltipProps) => {
-	if (!payload || !label || !date) return null;
+const ChartTooltip = React.memo(
+	({ label, date, period, payload, valueFormatter }: ChartTooltipProps) => {
+		if (!payload || !label || !date) return null;
 
-	let dateFormatter = intlFormatterBasic;
-	if (period === 'today' || period === 'yesterday' || period?.endsWith('h')) {
-		dateFormatter = intlFormatterAll;
-	}
-	if (period?.endsWith('d')) {
-		dateFormatter = intlFormatterDay;
-	}
+		const dateTimeFormat = useMemo(() => {
+			if (
+				period === PERIODS.TODAY ||
+				period === PERIODS.YESTERDAY ||
+				period?.endsWith('h')
+			) {
+				return intlFormatterAll;
+			}
 
-	return (
-		<Paper px="md" py="md" withBorder shadow="md" radius="md">
-			<Text fw={500} mb="xs">
-				{dateFormatter.format(parseISO(date))}
-			</Text>
-			{payload.map((item) => (
-				<Group key={item.name} gap="xs" justify="space-between">
-					<Group gap="sm">
-						<ColorSwatch color={item.color} size={12} withShadow={false} />
-						<Text key={item.name} fz="sm">
-							{label}
-						</Text>
+			if (period?.endsWith('d')) {
+				return intlFormatterDay;
+			}
+
+			return intlFormatterBasic;
+		}, [period]);
+
+		return (
+			<Paper px="md" py="md" withBorder shadow="md" radius="md">
+				<Text fw={500} mb="xs">
+					{dateTimeFormat.format(parseISO(date))}
+				</Text>
+				{payload.map((item) => (
+					<Group key={item.name} gap="xs" justify="space-between">
+						<Group gap="sm">
+							<ColorSwatch color={item.color} size={12} withShadow={false} />
+							<Text fz="sm">{label}</Text>
+						</Group>
+						<Text fz="sm">{valueFormatter(item.value)}</Text>
 					</Group>
-					<Text key={item.name} fz="sm">
-						{valueFormatter(item.value)}
-					</Text>
-				</Group>
-			))}
-		</Paper>
-	);
-};
+				))}
+			</Paper>
+		);
+	},
+);
 
 const BarChart = ({ label, data }: BarChartProps) => {
 	const [searchParams] = useSearchParams();
-	const period = searchParams.get('period');
+	const period = searchParams.get('period') as Period | null;
 
-	let dateFormatter = (date: Date) => format(date, 'MMM, yyyy');
-	switch (true) {
-		case period === null:
-		case period === undefined:
-		case period === 'today':
-		case period === 'yesterday':
-		case period?.endsWith('h'): {
-			dateFormatter = (date: Date) => format(date, 'HH:mm');
-			break;
+	const dateFormatter = useMemo(() => {
+		switch (true) {
+			case period === null:
+			case period === undefined:
+			case period === PERIODS.TODAY:
+			case period === PERIODS.YESTERDAY:
+			case period?.endsWith('h'): {
+				return (date: Date) => format(date, 'HH:mm');
+			}
+			case period?.endsWith('d') && Number.parseInt(period) <= 7: {
+				return (date: Date) => format(date, 'EEEEEE, MMM d');
+			}
+			case period?.endsWith('d') && Number.parseInt(period) > 7:
+			case period === PERIODS.QUARTER: {
+				return (date: Date) => format(date, 'MMM d');
+			}
 		}
-		case period?.endsWith('d') && Number.parseInt(String(period)) <= 7: {
-			dateFormatter = (date: Date) => format(date, 'EEEEEE, MMM d');
-			break;
-		}
-		case period?.endsWith('d') && Number.parseInt(String(period)) > 7:
-		case period === 'quarter': {
-			dateFormatter = (date: Date) => format(date, 'MMM d');
-			break;
-		}
-	}
 
-	const isDuration = label === 'Time Spent';
-	const isPercentage = label === 'Bounce Rate';
-	const valueFormatter = isDuration
-		? formatDuration
-		: isPercentage
-			? formatPercentage
-			: formatCount;
+		return (date: Date) => format(date, 'MMM, yyyy');
+	}, [period]);
+
+	const valueFormatter = useMemo(() => {
+		if (label === 'Time Spent') return formatDuration;
+		if (label === 'Bounce Rate') return formatPercentage;
+		return formatCount;
+	}, [label]);
 
 	return (
 		<MantineBarChart
