5b165eebaef55978344cf3b6a1ce420a6a8c7c89
Ayu <hello@ayuhito.com>
2024-06-30 17:18:24 +0100

    feat(dashboard): select referrer via external button (#68)

---
+++ dashboard/app/components/icons/external.tsx
@@ -0,0 +1,20 @@
+export const IconExternal = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<title>Icon External</title>
+			<path d="M15 3h6v6" />
+			<path d="M10 14 21 3" />
+			<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+		</svg>
+	);
+};
