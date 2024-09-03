ad18ab751811ceafbc30e6bb17f071c9d587ec9b
Ayuhito <hello@ayuhito.com>
2024-09-03 17:22:10 +0300

    fix(core): cors restricts custom headers unless specified

--- core/middlewares/commit.go
+++ core/middlewares/commit.go
@@ -10,7 +10,8 @@ func XAPICommitMiddleware(commit string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Include all routes except for /event routes.
-			if !strings.HasPrefix(r.URL.Path, "/api/event") {
+			// Only apply to GET requests.
+			if r.Method == http.MethodGet && !strings.HasPrefix(r.URL.Path, "/api/event") {
 				w.Header().Set("x-api-commit", commit)
 			}
 
