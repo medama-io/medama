b9c913f15a14606666dda7dc77ed7e4783099798
Ayu <hello@ayuhito.com>
2025-03-23 16:54:33 +0900

    feat(core): add automatic ssl provisioning (#155)
    * feat(core): add automatic ssl provisioning
    
    * fix(core): redirect to https globally

--- core/cmd/start.go
+++ core/cmd/start.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
+	"net"
 	"net/http"
 	"net/http/pprof"
 	"os"
 	"os/signal"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
+	"time"
 
+	"github.com/caddyserver/certmagic"
 	"github.com/go-faster/errors"
 	generate "github.com/medama-io/medama"
 	"github.com/medama-io/medama/api"
@@ -22,6 +27,13 @@ import (
 	"github.com/medama-io/medama/util"
 	"github.com/medama-io/medama/util/logger"
 	"github.com/ogen-go/ogen/middleware"
+	"github.com/rs/zerolog"
+)
+
+const (
+	HTTPPort        = 80
+	HTTPSPort       = 443
+	shutdownTimeout = 5 * time.Second
 )
 
 type StartCommand struct {
@@ -62,6 +74,10 @@ func (s *StartCommand) ParseFlags(args []string) error {
 	fs.Int64Var(&s.Server.Port, "port", s.Server.Port, "Port to listen on.")
 	fs.StringVar(&s.Server.Logger, "logger", s.Server.Logger, "Logger format (json, pretty)")
 	fs.StringVar(&s.Server.Level, "level", s.Server.Level, "Logger level (debug, info, warn, error)")
+
+	// AutoSSL settings.
+	fs.StringVar(&s.Server.AutoSSLDomain, "autossl", s.Server.AutoSSLDomain, "Automatically provision SSL certificates for the specified domain and redirect HTTP requests to HTTPS.\n\nRequires the server to run on ports 80 and 443. The domain must be publicly accessible and resolve to the server.")
+	fs.StringVar(&s.Server.AutoSSLEmail, "autosslemail", s.Server.AutoSSLEmail, "Email address to optionally send SSL certificate notifications.")
 
 	// Database settings.
 	fs.StringVar(&s.AppDB.Host, "appdb", s.AppDB.Host, "Path to app database.")
@@ -170,39 +186,171 @@ func (s *StartCommand) Run(ctx context.Context) error {
 
 	// Apply custom CORS middleware to the mux handler
 	handler := middlewares.CORSAllowedOriginsMiddleware(s.Server.CORSAllowedOrigins)(mux)
+
 	// X-API-Commit header for client-side cache busting.
 	handler = middlewares.XAPICommitMiddleware(s.Server.Commit)(handler)
 
-	srv := &http.Server{
-		Addr:         ":" + strconv.FormatInt(s.Server.Port, 10),
-		Handler:      handler,
-		ReadTimeout:  s.Server.TimeoutRead,
-		WriteTimeout: s.Server.TimeoutWrite,
-		IdleTimeout:  s.Server.TimeoutIdle,
+	return s.serve(ctx, log, handler)
+}
+
+// serve starts the HTTP server with the given handler. If AutoSSL is enabled, it will also provision certificates
+// and redirect HTTP to HTTPS.
+func (s *StartCommand) serve(ctx context.Context, log zerolog.Logger, mux http.Handler) error {
+	var (
+		httpListener  net.Listener
+		httpsListener net.Listener
+		httpWg        sync.WaitGroup
+
+		cfg *certmagic.Config
+	)
+
+	isSSL := s.Server.AutoSSLDomain != ""
+
+	// If AutoSSL is enabled, we need to provision SSL certificates for the domain and redirect HTTP to HTTPS.
+	// Otherwise, we can start the server as is.
+	if isSSL {
+		certmagic.DefaultACME.Agreed = true
+
+		if s.Server.AutoSSLEmail != "" {
+			certmagic.DefaultACME.Email = s.Server.AutoSSLEmail
+		}
+
+		cfg = certmagic.NewDefault()
+
+		err := cfg.ManageSync(ctx, []string{s.Server.AutoSSLDomain})
+		if err != nil {
+			return errors.Wrap(err, "failed to provision ssl certificate")
+		}
 	}
 
-	// Graceful shutdown
-	closed := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
-		<-sigint
+	// Create HTTP/S listeners.
+	httpPort := ":" + strconv.FormatInt(s.Server.Port, 10)
+	if isSSL {
+		httpPort = ":" + strconv.FormatInt(HTTPPort, 10)
+	}
 
+	httpListener, err := net.Listen("tcp", httpPort)
+	if err != nil {
+		return errors.Wrap(err, "failed to create http listener")
+	}
+
+	if isSSL {
+		httpsPort := ":" + strconv.FormatInt(HTTPSPort, 10)
+
+		tlsConfig := cfg.TLSConfig()
+		tlsConfig.NextProtos = append([]string{"h2", "http/1.1"}, tlsConfig.NextProtos...)
+
+		httpsListener, err = tls.Listen("tcp", httpsPort, tlsConfig)
+		if err != nil {
+			httpListener.Close()
+			return errors.Wrap(err, "failed to create https listener")
+		}
+	}
+
+	httpWg.Add(1)
+	defer httpWg.Done()
+
+	// Cleanup listeners when all servers are done.
+	go func() {
+		httpWg.Wait()
+		httpListener.Close()
+		if isSSL {
+			httpsListener.Close()
+		}
+	}()
+
+	var (
+		httpServer  *http.Server
+		httpsServer *http.Server
+	)
+
+	if !isSSL {
+		// If AutoSSL is not enabled, apply automatic redirection to HTTPS.
+		mux = middlewares.HTTPSRedirect(isSSL)(mux)
+
+		httpServer = &http.Server{
+			Addr:              ":" + strconv.FormatInt(s.Server.Port, 10),
+			Handler:           mux,
+			ReadHeaderTimeout: s.Server.TimeoutReadHeader,
+			ReadTimeout:       s.Server.TimeoutRead,
+			WriteTimeout:      s.Server.TimeoutWrite,
+			IdleTimeout:       s.Server.TimeoutIdle,
+			BaseContext:       func(_ net.Listener) context.Context { return ctx },
+		}
+	} else {
+		// The HTTP server solves the ACME challenges and redirects to HTTPS.
+		httpServer = &http.Server{
+			ReadHeaderTimeout: shutdownTimeout,
+			ReadTimeout:       shutdownTimeout,
+			WriteTimeout:      shutdownTimeout,
+			IdleTimeout:       shutdownTimeout,
+			BaseContext:       func(_ net.Listener) context.Context { return ctx },
+		}
+
+		if len(cfg.Issuers) > 0 {
+			if am, ok := cfg.Issuers[0].(*certmagic.ACMEIssuer); ok {
+				httpServer.Handler = am.HTTPChallengeHandler(middlewares.HTTPSRedirectFunc(true))
+			}
+		}
+
+		// The HTTPS server simply serves the user's handler.
+		httpsServer = &http.Server{
+			Handler:           mux,
+			ReadHeaderTimeout: s.Server.TimeoutReadHeader,
+			ReadTimeout:       s.Server.TimeoutRead,
+			WriteTimeout:      s.Server.TimeoutWrite,
+			IdleTimeout:       s.Server.TimeoutIdle,
+			BaseContext:       func(_ net.Listener) context.Context { return ctx },
+		}
+	}
+
+	// Graceful shutdown with signal handling.
+	closed := make(chan struct{})
+
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+		<-stop
 		log.Info().Msg("Shutting down server...")
 
-		ctx, cancel := context.WithTimeout(ctx, s.Server.TimeoutIdle)
+		shutdownCtx, cancel := context.WithTimeout(ctx, shutdownTimeout)
 		defer cancel()
 
-		if err := srv.Shutdown(ctx); err != nil {
-			log.Error().Err(err).Msg("Could not gracefully shutdown the server")
+		if isSSL {
+			if err := httpsServer.Shutdown(shutdownCtx); err != nil {
+				log.Error().Err(err).Msg("Could not gracefully shutdown the HTTPS server")
+			}
+		}
+
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			log.Error().Err(err).Msg("Could not gracefully shutdown the HTTP server")
 		}
 
 		close(closed)
 	}()
 
-	log.Info().Msgf("Starting server at http://localhost:%d", s.Server.Port)
-	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-		log.Panic().Err(err).Msgf("Could not listen on port: %d", s.Server.Port)
+	if !isSSL {
+		log.Info().Msgf("Starting server at http://localhost:%d", s.Server.Port)
+
+		if err := httpServer.Serve(httpListener); err != http.ErrServerClosed {
+			return errors.Wrap(err, "could not listen on port")
+		}
+
+		<-closed
+		return nil
+	}
+
+	log.Printf("Starting server with automatic SSL for %s configured.\n\nServing HTTP->HTTPS on %s and %s", s.Server.AutoSSLDomain, httpListener.Addr(), httpsListener.Addr())
+
+	go func() {
+		if err := httpServer.Serve(httpListener); err != http.ErrServerClosed {
+			log.Error().Err(err).Msg("Could not listen on http port")
+		}
+	}()
+
+	if err := httpsServer.Serve(httpsListener); err != http.ErrServerClosed {
+		return errors.Wrap(err, "could not listen on https port")
 	}
 
 	<-closed
