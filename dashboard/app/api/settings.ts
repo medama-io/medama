cdb1a1f0cff9b37ebc24e6bfc9ab9cc0f3fdccf4
tonievalue <maciej@muliszewski.com>
2026-06-06 12:57:44 +0200

    feat: switch sttings to use system settings

---
+++ dashboard/app/api/settings.ts
@@ -0,0 +1,15 @@
+import { type ClientOptions, client, type DataResponse } from "./client";
+
+export const systemSettingsGet = async (
+  opts?: ClientOptions,
+): Promise<DataResponse<"SystemSettings">> => {
+  const res = await client("/system/settings", { method: "GET", ...opts });
+  return { data: await res.json(), res };
+};
+
+export const systemSettingsUpdate = async (
+  opts: ClientOptions<"SystemSettings">,
+): Promise<DataResponse<"SystemSettings">> => {
+  const res = await client("/system/settings", { method: "PATCH", ...opts });
+  return { data: await res.json(), res };
+};
